94c4d0f0ab4e08e2f604ee0380e3cc24bdc195e3
HuaYang <HuaYang@host.docker.internal>
2020-04-13 21:09:13 +0800

    增加bootstrap 头部样式

--- src/main/java/com/yh/community/controller/IndexController.java
+++ src/main/java/com/yh/community/controller/IndexController.java
@@ -1,16 +1,13 @@
 package com.yh.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class IndexController {
-
-	@GetMapping("/index")
-	public String hello(@RequestParam(name = "name") String name, Model model) {
-		model.addAttribute("name", name);
+	@GetMapping("/")
+	public String index() {		
 		return "index";
 		}
 }
