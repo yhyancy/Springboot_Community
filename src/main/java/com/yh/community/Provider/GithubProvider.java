488f58b18fd0dfe813e38fb1bd2dd01a5f75e898
HuaYang <HuaYang@host.docker.internal>
2020-04-15 02:03:15 +0800

    submit

--- src/main/java/com/yh/community/Provider/GithubProvider.java
+++ src/main/java/com/yh/community/Provider/GithubProvider.java
@@ -1,8 +1,53 @@
 package com.yh.community.Provider;
 
+
 import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+import com.yh.community.dto.AccessTokenDTO;
+import com.yh.community.dto.GithubUser;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 @Component
 public class GithubProvider {
+public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+	 MediaType mediaType = MediaType.get("application/json; charset=utf-8");
 
+	 OkHttpClient client = new OkHttpClient();
+	 
+	 RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+	 Request request = new Request.Builder()
+			 .url("https://github.com/login/oauth/access_token")
+			 .post(body)
+			 .build();
+	 try (Response response = client.newCall(request).execute()) {
+		 String string= response.body().string();
+		 System.out.println(string);
+		 return string ;
+	 } catch (Exception e) {
+		// TODO: handle exception
+	}
+	 return null;
+	}
+public GithubUser getUser(String accessToken) {
+	OkHttpClient client = new OkHttpClient();
+	Request request = new Request.Builder()
+		      .url("https://api.github.com/user?access_token=" + accessToken)
+		      .build();
+
+		  try (Response response = client.newCall(request).execute()) {
+			String string= response.body().string();
+			GithubUser githubUser = JSON.parseObject(string,GithubUser.class);
+		    return githubUser ;		    
+		  } catch (Exception e) {
+			// TODO: handle exception
+		}
+		  return null;
+	
+}
 }
--- src/main/java/com/yh/community/controller/AuthorizeController.java
+++ src/main/java/com/yh/community/controller/AuthorizeController.java
@@ -1,13 +1,26 @@
 package com.yh.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.yh.community.Provider.GithubProvider;
+import com.yh.community.dto.AccessTokenDTO;
+
 @Controller
 public class AuthorizeController {
+	@Autowired
+	private GithubProvider githubProvider;
+	
 	@GetMapping("/callback")
 	public String callack(@RequestParam(name = "code" )String code, @RequestParam(name = "state") String state) {
+		AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+		accessTokenDTO.setClient_id("f2f38818b124625034a1");
+		accessTokenDTO.setClient_secret("a31a628c5bb45cec064800fbba56ef3204be37fa");
+		accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+		accessTokenDTO.setState(state);
+		githubProvider.getAccessToken(accessTokenDTO);
 		return "index";
 	}
 }
