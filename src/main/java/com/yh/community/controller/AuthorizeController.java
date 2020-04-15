2d9b4d431161fb7980a21971192e5272a112a5c5
HuaYang <HuaYang@9.200.45.226>
2020-04-15 23:39:09 +0800

    手动添加cookie

--- src/main/java/com/yh/community/controller/AuthorizeController.java
+++ src/main/java/com/yh/community/controller/AuthorizeController.java
@@ -2,7 +2,9 @@ package com.yh.community.controller;
 
 import java.util.UUID;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +17,8 @@ import com.yh.community.dto.AccessTokenDTO;
 import com.yh.community.dto.GithubUser;
 import com.yh.community.mapper.UserMapper;
 import com.yh.community.model.User;
+
+import okhttp3.Response;
 
 @Controller
 public class AuthorizeController {
@@ -32,7 +36,7 @@ public class AuthorizeController {
 
 	@GetMapping("/callback")
 	public String callack(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
-			HttpServletRequest request) {
+			HttpServletRequest request, HttpServletResponse response) {
 		AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 		accessTokenDTO.setClient_id(clientId);
 		accessTokenDTO.setClient_secret(clientSecret);
@@ -44,14 +48,18 @@ public class AuthorizeController {
 		System.out.println(githubUser.getName());
 		System.out.println(githubUser);
 		if (githubUser != null) {
-			User user= new User();			
-			user.setToken(UUID.randomUUID().toString());
+			User user= new User();	
+			String token= UUID.randomUUID().toString();
+			user.setToken(token);
 			user.setName(githubUser.getName());
 			user.setAccountId(String.valueOf(githubUser.getId()));
 			user.setGmtCreate(System.currentTimeMillis());
 			user.setGmtModified(user.getGmtCreate());
 			userMapper.insert(user);
-			request.getSession().setAttribute("user", githubUser);
+			//手动写cookie
+			response.addCookie(new Cookie("token", token));
+			
+//			request.getSession().setAttribute("user", githubUser);
 			return "redirect:/";
 		} else {
 			return "redirect:/";
--- src/main/java/com/yh/community/controller/IndexController.java
+++ src/main/java/com/yh/community/controller/IndexController.java
@@ -9,5 +9,6 @@ public class IndexController {
 	@GetMapping("/")
 	public String index() {		
 		return "index";
+		
 		}
 }
